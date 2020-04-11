85c980e0b655bad3b84947f04b904e406acce430
Mohammed Samour <60660377+Mohammed-Samour-pr@users.noreply.github.com>
2020-04-11 23:12:49 +0300

    Student__app1

---
+++ src/Q1/B.java
@@ -0,0 +1,15 @@
+package Q1;
+
+import java.util.function.Function;
+
+/**
+ *
+ * @author 
+ */
+public class B {
+
+    public static void main(String[] args) {
+        Function<String, String> toUpper_lambda = String::toUpperCase;
+        System.out.println(toUpper_lambda.apply("fgdfg"));
+    }
+}
